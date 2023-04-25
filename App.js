e4976470d808ee38b43d00a9c8e0a48103296af3
Chris Harris <chopuk@gmail.com>
2023-04-25 14:07:34 +0100

    Initial

--- App.js
+++ App.js
@@ -1,13 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { Alert, Button, StyleSheet, Text, View } from 'react-native'
+import ENVIRONMENT from './environment'
+import * as SplashScreen from 'expo-splash-screen'
+
+// keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
+
+  const [secureKeys,setSecureKeys] = useState()
+
+  useEffect(() => {
+
+    const fetchSecureKeys = async () => {
+
+      const appName = 'My Beaugtiful App'
+      // sending request
+      const response = await fetch(
+        `${ENVIRONMENT.URL_PREFIX}/getkeys`, {
+        method: "POST",
+        body: JSON.stringify({appName}),
+        headers: {
+          "Content-Type": "application/json",
+        }
+      })
+      const data = await response.json()
+      
+      if (!response.ok) return Alert.alert(data.message)
+      setSecureKeys(data)
+    }
+
+    fetchSecureKeys()
+
+  }, [])
+
+  // display the splash screen for 1 second...
+  setTimeout(async () => {
+    await SplashScreen.hideAsync()
+  }, 2000)
+
+  const amit = () => {
+    Alert.alert('Model: ' + secureKeys?.name)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="Press me"
+        onPress={() => amit()}
+      />
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +61,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-});
+  }
+})
